133f15f76d415e9ccb040ec7782811641a159d44
Handsome Yang <2577191892@qq.com>
2019-10-21 14:20:40 +0800

    添加cookie

--- logining/controller/userController.js
+++ logining/controller/userController.js
@@ -4,6 +4,15 @@ const user = require("../service/user");
 async function checkLogin(ctx, next) {  
     let {name, phone, password} = ctx.request.body;
     let data = await user.checkUser(phone, password);
+    if(data.code == 200){
+        ctx.cookies.set('cid', 'hello cookie', {
+            domian: 'localhost',
+            path: '/',
+            maxAge: 2*60*60*1000,
+            expires: new Date('2018-02-08'),
+            httpOnly: false
+        })
+    }
     return ctx.response.body = data;
 }
 // 注册
